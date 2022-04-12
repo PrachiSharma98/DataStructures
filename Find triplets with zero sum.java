e1ed2ad9a1fd42f4541929397779645489c73128
PrachiSharma98 <99522178+PrachiSharma98@users.noreply.github.com>
2022-04-12 09:59:57 +0530

    Create Find triplets with zero sum.java

---
+++ Find triplets with zero sum.java
@@ -0,0 +1,65 @@
+/*Given an array arr[] of n integers. Check whether it contains a triplet that sums up to zero. 
+
+Example 1:
+
+Input: n = 5, arr[] = {0, -1, 2, -3, 1}
+Output: 1
+Explanation: 0, -1 and 1 forms a triplet
+with sum equal to 0.
+Example 2:
+
+Input: n = 3, arr[] = {1, 2, 3}
+Output: 0
+Explanation: No triplet with zero sum exists.*/
+//Geeks for Geeks autocode 
+
+  import java.util.*;
+class Triplets{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+			int n=sc.nextInt();
+			int[] a=new int[n];
+			for(int i=0;i<n;i++){
+				a[i]=sc.nextInt();
+			}
+			Solution g=new Solution();
+			if(g.findTriplets(a,n))
+				System.out.println("1");
+			else
+				System.out.println("0");
+			
+		}
+	}
+}// } Driver Code Ends
+
+
+/*Complete the function below*/
+
+
+class Solution
+{
+    // arr[]: input array
+    // n: size of the array
+    //Function to find triplets with zero sum.
+	public boolean findTriplets(int arr[] , int n)
+    {
+        //add code here.
+        
+           if (n >= 3){
+            for(int i=0; i<n;i++){
+                for(int j=i+1; j<n;j++){
+                    for(int k=j+1; k<n;k++){
+                        if(arr[i]+arr[j]+arr[k] == 0) return true;
+                    }
+                }
+            }
+        }
+        return false;
+    
+        
+    }
+}
+
+
